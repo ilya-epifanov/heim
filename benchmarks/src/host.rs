3b9ddad89432f3249f9a8b92baa31f46acdf18c1
svartalf <self@svartalf.info>
2020-04-16 00:21:22 +0300

    Fix benchmarks

--- benchmarks/src/host.rs
+++ benchmarks/src/host.rs
@@ -1,14 +1,14 @@
 use criterion::{criterion_group, Criterion};
 
 pub fn inner(c: &mut Criterion) {
-    c.bench_function("host_platform", |b| b.iter(|| heim::host::platform()));
+    c.bench_function("host_platform", |b| b.iter(heim::host::platform));
 
-    c.bench_function("host_uptime", |b| b.iter(|| heim::host::uptime()));
+    c.bench_function("host_uptime", |b| b.iter(heim::host::uptime));
 
-    c.bench_function("host_boot_time", |b| b.iter(|| heim::host::boot_time()));
+    c.bench_function("host_boot_time", |b| b.iter(heim::host::boot_time));
 
     c.bench_function("host_users", |b| {
-        b.iter(|| heim::host::users().for_each(|_| ()))
+        b.iter(|| heim::host::users().unwrap().for_each(|_| ()))
     });
 }
 
--- benchmarks/src/memory.rs
+++ benchmarks/src/memory.rs
@@ -1,9 +1,9 @@
 use criterion::{criterion_group, Criterion};
 
 pub fn inner(c: &mut Criterion) {
-    c.bench_function("memory_memory", |b| b.iter(|| heim::memory::memory()));
+    c.bench_function("memory_memory", |b| b.iter(heim::memory::memory));
 
-    c.bench_function("memory_swap", |b| b.iter(|| heim::memory::swap()));
+    c.bench_function("memory_swap", |b| b.iter(heim::memory::swap));
 }
 
 criterion_group!(bench, inner);
