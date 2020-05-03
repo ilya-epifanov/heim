2dca025703a2f074514603b6aee4a58b9b8777a7
svartalf <self@svartalf.info>
2020-05-03 17:53:04 +0300

    Using smol for heim-memory

---
+++ heim-rt/src/fs.rs
@@ -0,0 +1,25 @@
+use std::io;
+use std::fs;
+use std::str::FromStr;
+use std::path::Path;
+
+use crate::spawn_blocking;
+
+pub async fn read_to_string<T>(path: T) -> io::Result<String> where T: AsRef<Path> + Send + 'static {
+    spawn_blocking(move ||  {
+        fs::read_to_string(path)
+    }).await
+}
+
+pub async fn read_into<T, R, E>(path: T) -> Result<R, E>
+where
+    T: AsRef<Path> + Send + 'static,
+    R: FromStr + Send + 'static,
+    E: From<io::Error> + From<<R as FromStr>::Err> + Send + 'static,
+{
+    spawn_blocking(|| {
+        let contents = fs::read_to_string(path)?;
+
+        R::from_str(&contents).map_err(Into::into)
+    }).await
+}
