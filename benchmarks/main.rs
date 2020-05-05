a79942da9602cb0af0df54a86f04a600a0166139
svartalf <self@svartalf.info>
2020-05-05 21:02:32 +0300

    benches

--- benchmarks/main.rs
+++ benchmarks/main.rs
@@ -1,7 +1,7 @@
 criterion::criterion_main!(
-    //    benchmarks::cpu::bench,
-    //    benchmarks::disk::bench,
-    //    benchmarks::host::bench,
+    benchmarks::cpu::bench,
+    benchmarks::disk::bench,
+    benchmarks::host::bench,
     benchmarks::memory::bench,
     //    benchmarks::net::bench,
     //    benchmarks::process::bench,
--- benchmarks/src/lib.rs
+++ benchmarks/src/lib.rs
@@ -1,9 +1,9 @@
 //! `[lib]` part contains a bunch of criterion groups,
 //! benchmarking each one of the `heim` components.
 
-//pub mod cpu;
-//pub mod disk;
-//pub mod host;
+pub mod cpu;
+pub mod disk;
+pub mod host;
 pub mod memory;
 //pub mod net;
 //pub mod process;
